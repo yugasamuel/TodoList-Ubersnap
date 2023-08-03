89df09aee2278163ae02a53fdb325e7791646d0d
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-03 22:24:46 +0700

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yuga Samuel on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
